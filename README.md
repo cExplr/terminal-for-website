05ab552f9642eea135b691d005a35980e99579a8
cExplr <code.explore.95@gmail.com>
2018-05-17 06:37:53 +0800

    Got the looks done. No enter button   a separate command input

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # This is the code that will be used in my website to simulate a command prompt or shell
+---
+
+
---
+++ terminal.js
@@ -0,0 +1,8 @@
+var inputData = ""
+var guestAtCexplr = "guest@c3xp1r ~ "
+var cursor = "|";
+
+
+//https://www.w3schools.com/jsref/obj_keyboardevent.asp
+//https://stackoverflow.com/questions/3149362/capture-key-press-or-keydown-event-on-div-element
+
